fc9d0dff64fee159f1f8fec3bb5419e14b62efb3
Warren Milburn <warren@munki.net>
2019-07-10 20:22:24 +0100

    Removed incorrect attribution comment.

--- src/net/munki/play/dep_injection/Lexicon.java
+++ src/net/munki/play/dep_injection/Lexicon.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.munki.play.dep_injection;
 
-/**
- *
- * @author Warren
- */
 public class Lexicon {
     
 }
--- src/net/munki/play/dep_injection/SpellChecker.java
+++ src/net/munki/play/dep_injection/SpellChecker.java
@@ -1,20 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.munki.play.dep_injection;
 
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Objects;
 
-/**
- *
- * @author Warren
- */
 public class SpellChecker {
-    private final Lexicon dictionary;
+    @SuppressWarnings("unused")
+	private final Lexicon dictionary;
     
     public SpellChecker(Lexicon dictionary) {
         this.dictionary = Objects.requireNonNull(dictionary);
